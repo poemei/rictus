b47496772fe9140662532104ad0133b064544900
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-23 10:22:26 -0700

    Connect Rictus through native platform sockets

--- build.cmd
+++ build.cmd
@@ -10,7 +10,7 @@ if errorlevel 1 (
 
 if not exist build\windows mkdir build\windows
 
-cl /nologo /std:c17 /W4 /TC /Iinclude /Fo"build\windows\\" /Fe"build\windows\rictus.exe" src\main.c src\rictus.c src\config.c
+cl /nologo /std:c17 /W4 /TC /Iinclude /Fo"build\windows\\" /Fe"build\windows\rictus.exe" src\main.c src\rictus.c src\config.c platforms\windows\rictus_net_windows.c /link ws2_32.lib
 if errorlevel 1 exit /b 1
 
 echo BUILD SUCCESSFUL
