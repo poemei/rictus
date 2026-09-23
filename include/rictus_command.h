b34ed49909043aea08bb3fc4f144d0ace0e858a9
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-23 11:12:36 -0700

    Bound command names and arguments

--- include/rictus_command.h
+++ include/rictus_command.h
@@ -5,15 +5,20 @@
 
 #include "rictus_dispatch.h"
 
+#define RICTUS_COMMAND_NAME_MAX 64
+#define RICTUS_COMMAND_ARGS_MAX RICTUS_IRC_PARAM_MAX
+
 typedef enum rictus_command_type {
     RICTUS_COMMAND_NONE = 0,
     RICTUS_COMMAND_STATUS,
+    RICTUS_COMMAND_HELP,
     RICTUS_COMMAND_UNKNOWN
 } rictus_command_type;
 
 typedef struct rictus_command {
     rictus_command_type type;
-    char name[64];
+    char name[RICTUS_COMMAND_NAME_MAX];
+    char arguments[RICTUS_COMMAND_ARGS_MAX];
 } rictus_command;
 
 int rictus_command_parse(const rictus_dispatch_result *dispatch,
