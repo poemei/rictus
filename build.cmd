e9f355ac2363040caa470d83e931cba725e241f8
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-23 18:06:33 -0700

    Build Windows IRC capability into module

--- build.cmd
+++ build.cmd
@@ -14,7 +14,7 @@ if not exist build\windows\modules mkdir build\windows\modules
 cl /nologo /std:c17 /W4 /TC /Iinclude /Fo"build\windows\\" /Fe"build\windows\rictus.exe" src\main.c src\rictus.c src\config.c src\irc_message.c src\event.c src\dispatch.c src\command.c src\module_inventory.c src\module_lifecycle.c ..\ABI\src\module.c ..\ABI\src\module_registry.c src\irc.c platforms\windows\rictus_net_windows.c platforms\windows\rictus_tls_windows.c /link ws2_32.lib secur32.lib crypt32.lib
 if errorlevel 1 exit /b 1
 
-cl /nologo /std:c17 /W4 /TC /Iinclude /Imodules\irc\include /LD modules\irc\src\irc_module.c /Fe"build\windows\modules\irc.dll"
+cl /nologo /std:c17 /W4 /TC /Iinclude /Imodules\irc\include /LD modules\irc\src\irc_module.c src\config.c src\irc.c src\irc_message.c src\event.c src\dispatch.c src\command.c platforms\windows\rictus_net_windows.c platforms\windows\rictus_tls_windows.c /Fe"build\windows\modules\irc.dll" /link ws2_32.lib secur32.lib crypt32.lib
 if errorlevel 1 exit /b 1
 
 echo BUILD SUCCESSFUL
