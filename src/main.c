12587c987130238a88e8c86984b0fed84c8ffc31
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-23 19:49:46 -0700

    Add deterministic module operator commands

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "rictus.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-    return rictus_run();
+    if (argc == 1) {
+        return rictus_run();
+    }
+
+    if (argc == 4 && strcmp(argv[1], "module") == 0) {
+        return rictus_module_control(argv[2], argv[3]);
+    }
+
+    fprintf(stderr,
+            "Usage: %s [module <status|enable|disable> <module-id>]\n",
+            argv[0]);
+    return 2;
 }
