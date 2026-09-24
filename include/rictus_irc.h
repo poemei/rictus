fd0e78fe12b4fc6b980626c00c6035d0a742755e
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-23 20:06:32 -0700

    Fix IRC online callback header

--- include/rictus_irc.h
+++ include/rictus_irc.h
@@ -7,11 +7,15 @@
 #include "rictus_module.h"
 #include "rictus_tls.h"
 
+typedef void (*rictus_irc_online_fn)(void *context);
+
 int rictus_irc_run(rictus_tls_connection *tls,
                    const rictus_irc_config *config,
                    char *error,
                    size_t error_size);
 
-typedef void (*rictus_irc_online_fn)(void *context);\n\nvoid rictus_irc_host_init(rictus_module_host_t *host);\nvoid rictus_irc_set_online_callback(rictus_irc_online_fn callback, void *context);
+void rictus_irc_host_init(rictus_module_host_t *host);
+void rictus_irc_set_online_callback(rictus_irc_online_fn callback,
+                                    void *context);
 
 #endif
