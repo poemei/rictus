4b9bc4766ddaab3f674b51d55c44fbd9e3b0c8db
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-23 18:05:56 -0700

    Tighten IRC module public contract

--- modules/irc/include/rictus_irc_module.h
+++ modules/irc/include/rictus_irc_module.h
@@ -3,13 +3,6 @@
 
 #include "rictus_module.h"
 
-/*
- * First real Rictus module.
- *
- * This establishes IRC as a module capability without yet changing the
- * proven runtime path. Core remains the authority for qualification and
- * activation.
- */
 const rictus_module_descriptor_t *rictus_irc_module_descriptor(void);
 
 #endif
