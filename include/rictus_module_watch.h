640a2c7a74791bef5531e077a9865bf6658cf450
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-23 19:34:40 -0700

    Give module watch Core lifecycle context

--- include/rictus_module_watch.h
+++ include/rictus_module_watch.h
@@ -4,6 +4,10 @@
 #include <pthread.h>
 
 #include "rictus_module_discovery.h"
+#include "rictus_module_inventory.h"
+#include "rictus_module_loader.h"
+#include "rictus_module_registry.h"
+#include "rictus_module_state.h"
 
 typedef struct rictus_module_watch {
     pthread_t thread;
@@ -11,12 +15,18 @@ typedef struct rictus_module_watch {
     rictus_module_candidate_t known[RICTUS_MODULE_LOADER_MAX];
     size_t known_count;
     int running;
+    rictus_module_loader_t *loader;
+    rictus_module_registry_t *registry;
+    rictus_module_store_t *state;
 } rictus_module_watch_t;
 
 int rictus_module_watch_start(
     rictus_module_watch_t *watch,
     const char *modules_path,
     const rictus_module_candidate_t *initial,
-    size_t initial_count);
+    size_t initial_count,
+    rictus_module_loader_t *loader,
+    rictus_module_registry_t *registry,
+    rictus_module_store_t *state);
 
 #endif
