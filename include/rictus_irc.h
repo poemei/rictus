fc910c98946322f003ff5c5a8145401a4f9bce5a
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-23 19:55:32 -0700

    Expose IRC-backed module host services

--- include/rictus_irc.h
+++ include/rictus_irc.h
@@ -4,11 +4,14 @@
 #include <stddef.h>
 
 #include "rictus_config.h"
+#include "rictus_module.h"
 #include "rictus_tls.h"
 
 int rictus_irc_run(rictus_tls_connection *tls,
                    const rictus_irc_config *config,
                    char *error,
                    size_t error_size);
+
+void rictus_irc_host_init(rictus_module_host_t *host);
 
 #endif
